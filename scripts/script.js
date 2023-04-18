9dc057797cd0296655e103be3b5c905cda111f53
Cailo Pinheiro <cailoisrael@hotmail.com>
2023-04-18 11:33:06 -0300

    add functions limparInput, limparOutput, copiar

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,7 @@ const buttonCodificar = document.getElementById("codificar");
 const buttonDecodificar = document.getElementById("decodificar");
 const normal = document.getElementById("normal");
 const codificada = document.getElementById("codificada");
+codificada.readOnly = true;
 let textoNormal = [];
 let textoCodificado = [];
 let textoDescodificado = [];
@@ -30,6 +31,8 @@ document.addEventListener("DOMContentLoaded", function () {
       alert(
         "ERRO: Certifique-se que sua mensagem não tenha letras maiúsculas, números, acentuação ou caracteres especiais."
       );
+      normal.value = "";
+      codificada.value = "";
       return;
     }
 
@@ -51,6 +54,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     }
     codificada.value = textoCodificado;
+    swap();
   });
 });
 
@@ -75,22 +79,47 @@ document.addEventListener("DOMContentLoaded", function () {
       .replace(/ober/g, ober)
       .replace(/ufat/g, ufat);
     codificada.value = textoDescodificado;
+    swap();
   });
 });
 
-// // adiciona um listener no botão limpar que limpa as textareas
-// const buttonLimpar = document.getElementById("limpar");
-// buttonLimpar.addEventListener("click", function limpar() {
-//   normal.value = "";
-//   codificada.value = "";
-// });
 
-// // adiciona um listener na textarea normal que limpa a textarea codificada
-// normal.addEventListener("input", function limparCodificada() {
-//   codificada.value = "";
-// });
+// quando a textarea de id codificada tiver vazia, exibe um conteúdo. Quando não estiver vazia, exibe outro conteúdo.
+function swap() {
+  const mensagem = document.getElementById("msg");
+  const codificada = document.getElementById("codificada");
 
-// // adiciona um listener na textarea codificada que limpa a textarea normal
-// codificada.addEventListener("input", function limparNormal() {
-//   normal.value = "";
-// });
+  if (!(codificada.value == "")) {
+    let contentNormal =
+      "<h3>Compartilhe sua mensagem com algum amigo!</h3><lottie-player src='https://lottie.host/e5f839f3-e731-4565-a831-1540ee5e01e6/1m7DLfadrN.json'background='transparent' speed='1' loop autoplay></lottie-player>";
+    mensagem.innerHTML = contentNormal;
+  } else {
+    setTimeout(() => {
+      let contentCodificada =
+        "<h3>Ainda não tem nenhuma mensagem :/</h3><img src='assets/illustration.svg'><span>Digite um texto que você queira codificar ou descodificar.</span>";
+      mensagem.innerHTML = contentCodificada;
+    }, 500);
+  }
+}
+
+codificada.addEventListener("input", swap);
+swap();
+
+// copia o valor da textarea input
+function limparInput() {
+  normal.value = "";
+  swap();
+}
+
+// copia o valor da textarea output
+function limparOutput() {
+  codificada.value = "";
+  swap();
+}
+
+// copia o valor da textarea output
+function copiar() {
+  
+    navigator.clipboard.writeText(codificada.value);
+  
+}
